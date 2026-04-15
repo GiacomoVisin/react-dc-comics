2b99948c9f10a0f3d05bc9125635da2e25537d35
GiacomoVisin <killerboss1150@gmail.com>
2026-04-15 15:13:17 +0200

    finishing the header

--- vite-project/src/components/AppHeader.jsx
+++ vite-project/src/components/AppHeader.jsx
@@ -4,29 +4,36 @@ import logo from "../assets/img/dc-logo.png"
 
 function AppHeader() {
     return (
+        <>
+            <div className="container">
+                <div className="containerFlex">
+                    <div>
+                        <img id="bannerLogo" src={logo}></img>
+                    </div>
 
-        /*  <img src={logo}></img> */
-
-        <ul id="list">
-            <li> <a href="#">CHARACTERS</a> </li>
-            <li> <a href="#"> COMICS </a></li>
-            <li>  <a href="#">MOVIES</a></li>
-            <li>  <a href="#">TV </a></li>
-            <li>  <a href="#">GAMES</a> </li>
-            <li>  <a href="#">COLLECTIBLES</a></li>
-            <li>  <a href="#">VIDEOS</a> </li>
-            <li>  <a href="#">FANS</a> </li>
-            <li>  <a href="#">NEWS </a></li>
-            <li> <a href="#">SHOP</a></li>
-        </ul>
-
-
-
-
+                    <div>
+                        <ul id="list">
+                            <li> <a href="#">CHARACTERS</a> </li>
+                            <li> <a href="#" id="blue"> COMICS </a></li>
+                            <li>  <a href="#">MOVIES</a></li>
+                            <li>  <a href="#">TV </a></li>
+                            <li>  <a href="#">GAMES</a> </li>
+                            <li>  <a href="#">COLLECTIBLES</a></li>
+                            <li>  <a href="#">VIDEOS</a> </li>
+                            <li>  <a href="#">FANS</a> </li>
+                            <li>  <a href="#">NEWS </a></li>
+                            <li> <a href="#">SHOP</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
 
 
 
+
+
+        </>
     )
 }
 
