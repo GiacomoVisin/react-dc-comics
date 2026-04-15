b34a8da2d65d455a7f2c5ef97fac1c99601ae362
GiacomoVisin <killerboss1150@gmail.com>
2026-04-15 14:51:27 +0200

    starting the header

---
+++ vite-project/src/components/AppHeader.jsx
@@ -0,0 +1,33 @@
+
+import logo from "../assets/img/dc-logo.png"
+
+
+function AppHeader() {
+    return (
+
+        /*  <img src={logo}></img> */
+
+        <ul id="list">
+            <li> <a href="#">CHARACTERS</a> </li>
+            <li> <a href="#"> COMICS </a></li>
+            <li>  <a href="#">MOVIES</a></li>
+            <li>  <a href="#">TV </a></li>
+            <li>  <a href="#">GAMES</a> </li>
+            <li>  <a href="#">COLLECTIBLES</a></li>
+            <li>  <a href="#">VIDEOS</a> </li>
+            <li>  <a href="#">FANS</a> </li>
+            <li>  <a href="#">NEWS </a></li>
+            <li> <a href="#">SHOP</a></li>
+        </ul>
+
+
+
+
+
+
+
+
+    )
+}
+
+export default AppHeader
