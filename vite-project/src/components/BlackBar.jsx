19f5079d4debba07cba46068266f54cd614d4b20
GiacomoVisin <killerboss1150@gmail.com>
2026-04-16 11:39:15 +0200

    fixing code

--- vite-project/src/components/BlackBar.jsx
+++ vite-project/src/components/BlackBar.jsx
@@ -1,12 +1,12 @@
 function BlackBar() {
     return (
-        <>
+        
             <div className="blackbar">
                 <div className="container">
                     <h2> -- Content Goes Here -- </h2>
                 </div>
             </div>
-        </>
+      
     )
 }
 
