c4e6d42595078bd1262b00dade27051eb8a1a17b
GiacomoVisin <killerboss1150@gmail.com>
2026-04-18 17:46:05 +0200

    adding title to the card

--- vite-project/src/components/BlackBar.jsx
+++ vite-project/src/components/BlackBar.jsx
@@ -12,7 +12,7 @@ function BlackBar() {
                     {comics.map((item) => (
                         <div key={item.id} className="col-6 col-md-4 col-lg-2">
                             <div className="card w-60 mt-3  border-0 bg-transparent">
-                                <img src={item.thumb} alt={item.series} placeholder={item.description} className="img-fluid" />
+                                <img  title={item.title} src={item.thumb} alt={item.series} className="img-fluid" />
                                 <p className="text-white uppercase pt-2"> {item.series} </p>
                             </div>
                         </div>
