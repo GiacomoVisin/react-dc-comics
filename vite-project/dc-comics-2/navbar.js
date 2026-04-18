8e9b8e83fa3e47a8e3e4674d2f6f20ee26e16731
GiacomoVisin <killerboss1150@gmail.com>
2026-04-18 12:12:24 +0200

    addind js for navbar

---
+++ vite-project/dc-comics-2/navbar.js
@@ -0,0 +1,52 @@
+const NavBar = [
+    {
+        id:1,
+        name: "characters",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:2,
+        name: "comics",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:3,
+        name: "movies",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:4,
+        name: "tv",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:5,
+        name: "games",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:6,
+        name: "collectibles",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:7,
+        name: "videos",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:8,
+        name: "Fans",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:9,
+        name: "News",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    },
+    {
+        id:10,
+        name: "Shop",
+        link: "https://www.bing.com/ck/a?!&&p=60aa3458d2465f34c9f849a7eb493ac6cbf1707df86838dc1e6554a344c30549JmltdHM9MTc3NjQ3MDQwMA&ptn=3&ver=2&hsh=4&fclid=07ea50c7-ad1c-695c-3024-4631ac6b687c&psq=personaggi+dc&u=a1aHR0cHM6Ly93d3cuZGMuY29tL2NoYXJhY3RlcnM"
+    }
+]
