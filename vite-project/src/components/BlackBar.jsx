c9a8b910797495d767e973da638073477c3f60d2
GiacomoVisin <killerboss1150@gmail.com>
2026-04-18 12:07:03 +0200

    fixing the card

--- vite-project/src/components/BlackBar.jsx
+++ vite-project/src/components/BlackBar.jsx
@@ -7,11 +7,11 @@ function BlackBar() {
 
         <div className="blackbar">
             <div className="container">
-                <div className="row">
+                <div className="row g-1">
                     {comics.map((item) => (
                         <div key={item.id} className="col-6 col-md-4 col-lg-2">
-                            <div className="card w-60 mt-4 mb-1 border-0 bg-transparent">
-                                <img src={item.thumb} alt={item.series} className="img-fluid" />
+                            <div className="card w-60 mt-3  border-0 bg-transparent">
+                                <img src={item.thumb} alt={item.series} placeholder={item.description} className="img-fluid" />
                                  <p className="text-white uppercase pt-2"> {item.series} </p>
                             </div>
                         </div>
