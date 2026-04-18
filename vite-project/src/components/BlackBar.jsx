e91d7021c3557c2560d1b8122c335ace59a4c367
GiacomoVisin <killerboss1150@gmail.com>
2026-04-18 15:26:56 +0200

    adding buttons

--- vite-project/src/components/BlackBar.jsx
+++ vite-project/src/components/BlackBar.jsx
@@ -7,16 +7,18 @@ function BlackBar() {
 
         <div className="blackbar">
             <div className="container">
+                <button className="Series"> CURRENT SERIES </button>
                 <div className="row g-1">
                     {comics.map((item) => (
                         <div key={item.id} className="col-6 col-md-4 col-lg-2">
                             <div className="card w-60 mt-3  border-0 bg-transparent">
                                 <img src={item.thumb} alt={item.series} placeholder={item.description} className="img-fluid" />
-                                 <p className="text-white uppercase pt-2"> {item.series} </p>
+                                <p className="text-white uppercase pt-2"> {item.series} </p>
                             </div>
                         </div>
                     ))}
                 </div>
+                <button className="Loading"> LOAD MORE </button>
             </div>
         </div>
 
